37669b9b9b974eba1dcd10e3e6b98d7297c56072
Harshit <harshitchugh001@gmail.com>
2023-06-16 22:58:33 +0530

    first commit

---
+++ build/precache-manifest.85df4e18bb8769dd025415ebeb3636e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c97ac79655fd7cd393d5c5a43b2b7b51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80f3f06b465ca9afd1b9",
+    "url": "/static/css/2.352a0142.chunk.css"
+  },
+  {
+    "revision": "80f3f06b465ca9afd1b9",
+    "url": "/static/js/2.284040e8.chunk.js"
+  },
+  {
+    "revision": "bf47970d488efafab481",
+    "url": "/static/js/main.8e438eb9.chunk.js"
+  },
+  {
+    "revision": "c20bda0da91af47bb8f6",
+    "url": "/static/js/runtime-main.8fc0d6a4.js"
+  }
+]);
