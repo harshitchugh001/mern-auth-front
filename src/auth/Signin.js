f7fd27734bf261b48f04c01ff033ef46790847f0
Harshit <harshitchugh001@gmail.com>
2023-06-17 08:28:22 +0530

    change

--- src/auth/Signin.js
+++ src/auth/Signin.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import { authenticate, isAuth } from './helpers';
 import { ToastContainer, toast } from 'react-toastify';
 import Google from './Google';
-import Facebook from './Facebook';
+// import Facebook from './Facebook';
 import 'react-toastify/dist/ReactToastify.min.css';
 
 const Signin = ({ history }) => {
@@ -79,7 +79,7 @@ const Signin = ({ history }) => {
                 {isAuth() ? <Redirect to="/" /> : null}
                 <h1 className="p-5 text-center">Signin</h1>
                 <Google informParent={informParent} />
-                <Facebook informParent={informParent} />
+                {/* <Facebook informParent={informParent} /> */}
                 {signinForm()}
                 <br />
                 <Link to="/auth/password/forgot" className="btn btn-sm btn-outline-danger">
